1923ad80630054f80a1be59cb8b4c6582c39bee5
Patrick Gagnon-Renaud <pgrenaud@exolnet.com>
2018-08-17 11:21:08 -0400

    Add tests

---
+++ tests/Integration/BackupDatabaseCommandTest.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace Exolnet\Backup\Tests\Integration;
+
+use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\Artisan;
+
+class BackupDatabaseCommandTest extends TestCase
+{
+    /** @var \Illuminate\Support\Carbon */
+    protected $date;
+
+    /** @var string */
+    protected $expectedDumpPath;
+
+    public function setUp()
+    {
+        parent::setUp();
+
+        $this->date = Carbon::create(2018, 8, 15, 12, 30, 59);
+
+        Carbon::setTestNow($this->date);
+
+        $this->expectedDumpPath = $this->getExpectedDumpPath('forge', $this->date);
+    }
+
+    public function testCommandNoArguments()
+    {
+        $resultCode = Artisan::call('backup:database');
+
+        $this->assertEquals(0, $resultCode);
+        $this->assertFileExists($this->expectedDumpPath);
+    }
+
+    public function testCommandWithFilename()
+    {
+        $this->expectedDumpPath = 'custom_filename';
+
+        $resultCode = Artisan::call('backup:database', ['filename' => 'custom_filename']);
+
+        $this->assertEquals(0, $resultCode);
+        $this->assertFileExists($this->expectedDumpPath);
+    }
+
+    public function testCommandWithPath()
+    {
+        is_dir('some_dir') || mkdir('some_dir');
+
+        $this->expectedDumpPath = 'some_dir' . DIRECTORY_SEPARATOR . $this->expectedDumpPath;
+
+        $resultCode = Artisan::call('backup:database', ['--path' => 'some_dir']);
+
+        $this->assertEquals(0, $resultCode);
+        $this->assertFileExists($this->expectedDumpPath);
+    }
+
+    public function testCommandWithPathAndFilename()
+    {
+        is_dir('some_dir') || mkdir('some_dir');
+
+        $this->expectedDumpPath = 'some_dir' . DIRECTORY_SEPARATOR . 'custom_filename';
+
+        $resultCode = Artisan::call('backup:database', ['--path' => 'some_dir', 'filename' => 'custom_filename']);
+
+        $this->assertEquals(0, $resultCode);
+        $this->assertFileExists($this->expectedDumpPath);
+    }
+
+    public function testCommandWithConnection()
+    {
+        $connection = $this->app['config']->get('database.connections.mysql');
+        $connection['database'] = 'custom_database';
+
+        $this->app['config']->set('database.connections.custom_connection', $connection);
+
+        $resultCode = Artisan::call('backup:database', ['--connection' => 'custom_connection']);
+
+        $this->assertEquals(0, $resultCode);
+        $this->assertFileExists($this->getExpectedDumpPath('custom_database', $this->date));
+    }
+
+    /**
+     * @param string $database
+     * @param \Illuminate\Support\Carbon $date
+     * @return string
+     */
+    protected function getExpectedDumpPath($database, $date)
+    {
+        return "{$database}-{$date->format('Y-m-d_H-i-s')}.sql.gz";
+    }
+}
---
+++ tests/Integration/TestCase.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Exolnet\Backup\Tests\Integration;
+
+use Exolnet\Backup\BackupServiceProvider;
+use Orchestra\Testbench\TestCase as Orchestra;
+
+abstract class TestCase extends Orchestra
+{
+    /**
+     * @param \Illuminate\Foundation\Application $app
+     *
+     * @return array
+     */
+    protected function getPackageProviders($app)
+    {
+        return [
+            BackupServiceProvider::class,
+        ];
+    }
+}
